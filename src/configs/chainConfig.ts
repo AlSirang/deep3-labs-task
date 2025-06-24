89f62b9d0f59105bdf22bf92d9a4501cbb519063
Bashir Uddin <uddinbashir1999@gmail.com>
2025-06-25 04:53:54 +0500

    fix: remove metamask connector - use injected as default connector ETH

--- src/configs/chainConfig.ts
+++ src/configs/chainConfig.ts
@@ -1,6 +1,6 @@
 import { http, createConfig } from 'wagmi'
 import { mainnet, sepolia } from 'wagmi/chains'
-import { injected, metaMask, walletConnect } from 'wagmi/connectors'
+import { injected, walletConnect } from 'wagmi/connectors'
 import { createWeb3Modal } from '@web3modal/wagmi/react'
 
 const projectId = import.meta.env.VITE_WALLET_CONNECT_PROJECT_ID
@@ -9,7 +9,7 @@ const chains = [mainnet, sepolia] as const
 
 export const wagmiConfig = createConfig({
   chains,
-  connectors: [injected(), walletConnect({ projectId, showQrModal: false }), metaMask()],
+  connectors: [injected(), walletConnect({ projectId, showQrModal: false })],
   transports: {
     [mainnet.id]: http(),
     [sepolia.id]: http(),
